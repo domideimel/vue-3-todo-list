7b16283d6c79af0edf3470283e4d6c8964f402d6
dominikdeimel <info@dominikdeimel.com>
2022-02-19 21:48:48 +0100

    chore: added transition

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -10,10 +10,31 @@ const todos = computed<Todo[]>(() => todoStore.todos)
 </script>
 
 <template>
-  <ul
-    v-if="todos.length"
-    class="menu bg-base-100 menu-compact lg:menu-normal w-full p-2 rounded-box -mt-[25vmin] sm:-mt-[5min] md:-mt-[12vh] lg:-mt-[12vmin] shadow"
-  >
-    <todo-list-item v-for="todo in todos" :key="todo.id" :todo="todo"/>
-  </ul>
+  <transition>
+    <TransitionGroup
+      name="list"
+      tag="ul"
+      v-if="todos.length"
+      class="menu bg-base-100 menu-compact lg:menu-normal w-full p-2 rounded-box -mt-[25vmin] sm:-mt-[5min] md:-mt-[12vh] lg:-mt-[12vmin] shadow"
+    >
+      <todo-list-item v-for="todo in todos" :key="todo.id" :todo="todo"/>
+    </TransitionGroup>
+  </transition>
 </template>
+
+
+<style scoped>
+.v-enter-active,
+.v-leave-active,
+.list-enter-active,
+.list-leave-active {
+  transition: opacity 0.25s ease-in;
+}
+
+.v-enter-from,
+.v-leave-to,
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+}
+</style>
