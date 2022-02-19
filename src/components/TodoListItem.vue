c5b9ed7868fd63986e4b96cbcdf57ed08934836f
dominikdeimel <info@dominikdeimel.com>
2022-02-19 21:48:30 +0100

    chore: added delete Item

--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -17,20 +17,35 @@ const handleCheckedChange = (e: Event) => {
   localTodo.value.completed = (e.target as HTMLInputElement).checked
   todoStore.updateTodo(localTodo.value.id.toString(), localTodo.value)
 }
+
+const handleDelete = () => {
+  todoStore.deleteTodo(localTodo.value.id.toString())
+}
 </script>
 
 <template>
   <li>
-    <div class="form-control">
-      <label class="cursor-pointer label w-full">
-        <span class="label-text">{{ props.todo.title }}</span>
-        <input
-          type="checkbox"
-          :checked="props.todo.completed"
-          class="checkbox checkbox-secondary"
-          @change="handleCheckedChange"
-        >
-      </label>
+    <div class="indicator w-full bg-black/[.05] hover:bg-black/[0.15]">
+      <button
+        class="indicator-item badge badge-secondary w-6 h-6 p-1 rounded-full hover:bg-secondary-focus hover:border-secondary-focus"
+        @click="handleDelete"
+      >
+         <svg viewBox="0 0 24 24" class="w-3 h-3 fill-white">
+                <path
+                  d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"/>
+              </svg>
+      </button>
+      <div class="form-control w-full ">
+        <label class="cursor-pointer label w-full">
+          <span class="label-text">{{ props.todo.title }}</span>
+          <input
+            type="checkbox"
+            :checked="props.todo.completed"
+            class="checkbox checkbox-secondary"
+            @change="handleCheckedChange"
+          >
+        </label>
+      </div>
     </div>
   </li>
 </template>
