40dd450d9528be29b6f8d39effcd96cd5b9bde7d
dominikdeimel <info@dominikdeimel.com>
2022-02-19 21:32:09 +0100

    chore: updated todoList to only sho if there are todos

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,14 +2,18 @@
 import TodoListItem from './TodoListItem.vue'
 import useTodoStore from '../store/todo'
 import { computed } from 'vue'
+import { Todo } from '../types/todo'
 
 const todoStore = useTodoStore()
-const todos = computed(() => todoStore.todos)
+const todos = computed<Todo[]>(() => todoStore.todos)
 
 </script>
 
 <template>
-  <ul class="menu bg-base-100 menu-compact lg:menu-normal w-full p-2 rounded-box -mt-[25vmin] sm:-mt-[5min] md:-mt-[12vh] lg:-mt-[12vmin] shadow">
+  <ul
+    v-if="todos.length"
+    class="menu bg-base-100 menu-compact lg:menu-normal w-full p-2 rounded-box -mt-[25vmin] sm:-mt-[5min] md:-mt-[12vh] lg:-mt-[12vmin] shadow"
+  >
     <todo-list-item v-for="todo in todos" :key="todo.id" :todo="todo"/>
   </ul>
 </template>
